cf7bacfb687c5eb77d6f8b904e9201d52ce9533f
Nje-t <njerinjesh24@gmail.com>
2023-02-16 04:52:02 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always o (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
