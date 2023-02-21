d5333a49c842e0fcfd9c8fff730cc66e3e38bf2b
Nje-t <njerinjesh24@gmail.com>
2023-02-21 04:32:09 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,9 @@
 
 int print_last_digit(int n)
 {
-	int last;
-	last = n % 10;
-	if (last < 0)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+	if (n < 0)
+		n *= -1;
+	_putchar('0' + (n % 10));
+
+	return (n % 10);
 }
