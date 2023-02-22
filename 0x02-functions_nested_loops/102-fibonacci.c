0a845325c3b7ee9a89e3b7d97c4ff71ce8258855
Nje-t <njerinjesh24@gmail.com>
2023-02-22 14:26:57 -0800

    Task 14 fibs

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print first 50 Fibonacci numbers, starting with 1 and 2,
- * separated by a comma followed by a space.
+ *      separated by a comma followed by a space.
  *
  * Return: 0
  */
